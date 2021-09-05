4655c1282f8d4426d1d6a4b1548417ed2f97e98f
Hao-Wei Jeng <locky4567@gmail.com>
2021-09-05 18:51:57 +0800

    Update index.js

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const Hapi = require('@hapi/hapi');
 
 const init = async () => {
   const server = Hapi.server({
-    port: 3000,
-    host: 'localhost',
+    port: ~~process.env.PORT || 3000,
+    host: '0.0.0.0',
   });
 
   server.route({
